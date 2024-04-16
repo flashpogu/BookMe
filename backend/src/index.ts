a39d9357386e21be9e5ec8b4fcfef137cbb78100
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-16 17:25:55 +0530

    implemented cloudinary and set the form route for adding new hotel data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
+import myHotelRoutes from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -25,6 +33,7 @@ app.use(
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
